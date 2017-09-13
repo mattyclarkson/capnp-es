9d087fdb7410495cfb7ac18734ae0c6154592014
Matt Clarkson <mattyclarkson@gmail.com>
2017-09-13 12:37:38 +0100

    Add package information

---
+++ rollup.binary.config.js
@@ -0,0 +1,7 @@
+export default {
+  input: 'bin/capnpc-es.js',
+  output: {
+    file: 'dist/bin/capnpc-es.js',
+    format: 'cjs'
+  }
+};
---
+++ rollup.library.config.js
@@ -0,0 +1,7 @@
+export default {
+  input: 'lib/capnp-es.js',
+  output: {
+    file: 'dist/lib/capnp-es.js',
+    format: 'cjs'
+  }
+};
