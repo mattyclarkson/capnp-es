b743340f11d08e814e316ca8acae1b003e0b5f32
Matt Clarkson <mattyclarkson@gmail.com>
2017-09-13 16:31:37 +0100

    Make rolled up binary executable

--- rollup.binary.config.js
+++ rollup.binary.config.js
@@ -1,8 +1,20 @@
+import fs from 'fs';
+
+function chmod({mode} = {mode: '644'}) {
+  return {
+    name: 'json',
+    onwrite: ({file}) => {
+      fs.chmodSync(file, mode);
+    }
+  };
+}
+
 export default {
   input: 'bin/capnpc-es.js',
   output: {
     file: 'dist/bin/capnpc-es.js',
     format: 'cjs',
     banner: '#! /usr/bin/env node\n'
-  }
+  },
+  plugins: [chmod({mode: '755'})]
 };
