5fe3aff5100b9791a910bd36682447b32115d66c
Matt Clarkson <mattyclarkson@gmail.com>
2017-10-02 17:27:03 +0100

    Add fetching of compiled schema fixtures
    This allows the unit tests to fetch compiled schema files in binary
    format to perform unit tests on. This is implemented this way so that
    we will be able to perform the same unit tests in the browser. We
    ideally want all of our parsing and generation code to work in the
    browser so we can auto-generate API code if necessary

---
+++ test/fetchSchema.js
@@ -0,0 +1,35 @@
+/**
+ * Reports an error when fetching a URL
+ * @param {String} message the error message for the error
+ * @param {Response} response the result of the fetch request
+ */
+export class FetchError extends Error {
+  constructor(message, response) {
+    super(message);
+    Object.defineProperties(this, {
+      response: {value: response}
+    });
+  }
+}
+
+/**
+ * Fetches a compiled schema file
+ * @async
+ * @param {String} schema the schema file to load from the fixtures directory
+ * @throws {FetchError} when the fetching of the compiled schema fails
+ * @returns {Promise<ArrayBuffer>} the compiled binary data
+ */
+export default async function(schema) {
+  const headers = new Headers({'Accept': 'application/x-capnp-schema-binary'});
+  const response = await fetch(`${location.origin}/${schema}`, {headers});
+
+  if (!response.ok) {
+    const json = await response.json();
+    const {error} = json;
+    throw new FetchError(error, response);
+  }
+
+  return await response.arrayBuffer();
+}
+
+
--- test/mocha.js
+++ test/mocha.js
@@ -2,7 +2,11 @@ import chai from 'chai';
 import {URL} from 'url';
 import babelRegister from 'babel-register';
 import fs from 'fs';
+import http from 'http';
 import path from 'path';
+import fetch, {Headers, Request, Response} from 'node-fetch';
+import compile, {compiler} from '../lib/compile.js';
+import fetchSchema from './fetchSchema.js';
 
 const pkg = JSON.parse(fs.readFileSync(path.join(__dirname, '..', 'package.json')));
 const babelrc = JSON.parse(fs.readFileSync(path.join(__dirname, '..', '.babelrc')));
@@ -10,6 +14,52 @@ const ignoreRegexp = new RegExp(`node_modules/(?!${Object.keys(pkg.dependencies 
 const options = Object.assign(babelrc, {ignore: (filename) => filename.match(ignoreRegexp), babelrc: false});
 babelRegister(options);
 
+const port = 58956;
+
 global.chai = chai;
 global.should = chai.should();
 global.URL = URL;
+global.fetch = fetch;
+global.fetchSchema = fetchSchema;
+global.Headers = Headers;
+global.Request = Request;
+global.Response = Response;
+global.location = {
+  origin: `http://localhost:${port}`
+};
+
+function handleRequest(request, response) {
+  const {accept} = request.headers;
+  if (['application/x-capnp-schema-binary', 'application/*', '*/*'].indexOf(accept) === -1) {
+    response.writeHead(406, {'Content-Type': 'application/json'});
+    response.end(JSON.stringify({error: `Invalid accepts: ${accept}`}));
+  }
+
+  const url = new URL(`${location.origin}${request.url}`);
+  compiler()
+    .catch(err => {
+      response.writeHead(501, {'Content-Type': 'application/json'});
+      response.end(JSON.stringify({error: `Failed to find compiler: ${err}`}));
+    })
+    .then(executable => {
+      return compile(path.join(__dirname, 'fixtures', url.pathname), executable)
+        .then(schema => {
+          response.writeHead(200, {'Content-Type': 'application/x-capnp-schema-binary'});
+          response.end(Buffer.from(schema, 0, schema.length));
+        })
+        .catch(err => {
+          response.writeHead(500, {'Content-Type': 'application/json'});
+          response.end(JSON.stringify({error: `Failed to compile schema: ${err.message}`}));
+        });
+    });
+}
+
+before(function(done) {
+  this.port = port;
+  this.server = http.createServer(handleRequest);
+  this.server.listen(this.port, done);
+});
+
+after(function(done) {
+  this.server.close(done);
+});
