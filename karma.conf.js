87b295301e58803a3c0200d0567fa449b712683f
Matt Clarkson <mattyclarkson@gmail.com>
2017-10-10 16:58:12 +0100

    Only output Karma warnings

--- karma.conf.js
+++ karma.conf.js
@@ -4,6 +4,7 @@ module.exports = function(config) {
     files: [
       'test/spec/simple.js'
     ],
+    logLevel: config.LOG_WARN,
     browsers: ['ChromeHeadless'],
     reporters: ['mocha']
   });
