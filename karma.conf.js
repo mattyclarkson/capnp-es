bc0b3f8f795d7798af34db5a402e91edc81e9d33
Matt Clarkson <mattyclarkson@gmail.com>
2017-10-10 16:46:31 +0100

    Add a simple Karma test

---
+++ karma.conf.js
@@ -0,0 +1,9 @@
+module.exports = function(config) {
+  config.set({
+    frameworks: ['mocha', 'chai'],
+    files: [
+      'test/spec/simple.js'
+    ],
+    browsers: ['ChromeHeadless']
+  });
+};
